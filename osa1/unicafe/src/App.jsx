3009d602af4038d7ff0dc98e92b6b09dfbd17551
lassisav <lassi.savolainen@helsinki.fi>
2024-01-17 21:40:11 +0200

    Tehtävä 1.6.

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <p>give feedback</p>
+      <Button
+        handleClick={increaseGood}
+        text='Good'
+      />
+      <Button
+        handleClick={increaseNeutral}
+        text='Neutral'
+      />
+      <Button
+        handleClick={increaseBad}
+        text='Bad'
+      />
+      <p>statistics</p>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad} </p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+export default App
