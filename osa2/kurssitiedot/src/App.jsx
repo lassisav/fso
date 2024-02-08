7ef82bf578fae28d3e449d9d3ed1b2e9ab007626
lassisav <lassi.savolainen@helsinki.fi>
2024-02-08 19:33:27 +0200

    Tehtävä 2.4.

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,52 +1,82 @@
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <h1>
+        Web development curriculum
+      </h1>
+      <Courses courses={courses}/>
     </div>
   )
 }
 
+const Courses = (props) => {
+  return (
+    props.courses.map(temp =>
+      <div key={temp.id}>
+        <Course course={temp}/>
+      </div>)
+)
+}
+
 const Course = (props) => {
   return (
-    <div>
+    <li>
       <Header course={props.course.name} />
       <Content parts={props.course.parts} />
       <Total parts={props.course.parts}/>
-    </div>
+    </li>
   )
 }
 
 const Header = (props) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h3>{props.course}</h3>
     </>
   )
 }
@@ -55,7 +85,8 @@ const Content = (props) => {
   return(props.parts.map(temp =>
     <li key={temp.id}>
       <Part part={temp} />
-    </li>)
+    </li>
+    )
   )
 }
 
@@ -69,7 +100,6 @@ const Part = (props) => {
 
 const Total = (props) => {
   let result = props.parts.map(temp => temp.exercises)
-  console.log(result)
   return(
     <p><b>total of {result.reduce((a, b) => a + b, 0)} exercises</b></p>
   )
